e9047261442ece5d90bfe428e9faae3c12a079a5
GsmToday <gaoshimengdlnu@gmail.com>
2014-10-22 21:18:11 +0800

    practice 1

---
+++ Chapter5/Ex01.java
@@ -0,0 +1,14 @@
+/**
+ * For initialization
+ * Chapter 5 in Thinking in Java
+ * Practice 1
+ */
+package initializationandcleanup;
+
+public class Ex01 {
+	String s;
+	public static void main(String[] args) {
+		Ex01 e = new Ex01();
+		System.out.println(e.s);
+	}
+}
