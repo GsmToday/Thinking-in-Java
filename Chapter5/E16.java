afc4f0ad42c3265dc5129d6726ad7c0d2bec6101
GsmToday <gaoshimengdlnu@gmail.com>
2014-10-27 11:45:42 +0800

    practice 16

---
+++ Chapter5/E16.java
@@ -0,0 +1,15 @@
+/**
+ * For array initialization
+ * Chapter 5 in Thinking in Java
+ * Practice 16
+ */
+public class E16 {	
+	public static void main(String[] args) {
+		// aggregate initialization
+		String[]s = {"I" , "am", "GSM"};
+		for(int i = 0; i<s.length ;i++)
+		{
+			Prt.prt(s[i]);
+		}
+	}
+}
