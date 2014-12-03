0c8bd202bcc1da836f1b6c12230c042582dfbbcc
GsmToday <gaoshimengdlnu@gmail.com>
2014-12-03 10:15:10 +0800

    practice 14

--- Chapter11/Ex14.java
+++ Chapter11/Ex14.java
@@ -1,9 +1,7 @@
 /**
- * Inner class
- * Chapter 10 in Thinking in Java
+ * holding object
+ * Chapter 11 in Thinking in Java
  * Practice 14
- * inner classes have transparent access to their outer class
- * objects, even private fields and methods.
  */
 package holding;
 import java.util.*;
@@ -12,7 +10,8 @@ public class Ex14 {
 		// TODO Auto-generated method stub
 		LinkedList<Integer> list = new LinkedList();
 		//ListIterator<Integer> it = list.listIterator(); 
-		for(int i = 1;i<=10;i++){
+		for(int i = 0;i<10;i++){
+			System.out.println(""+i/2);
 			ListIterator<Integer> it = list.listIterator(i/2); 
 			it.add(i);
 		}
